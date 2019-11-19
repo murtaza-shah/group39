cd32f1bddacc0dbbfa6c24825e406d95ec72387e
Murtizzy <35986870+Murtizzy@users.noreply.github.com>
2019-11-19 13:30:04 -0700

    added tables

--- index.php
+++ index.php
@@ -28,6 +28,58 @@ if (!($result->num_rows > 0)) {
 	$connection->query($sql);
 }
 
+$sql = "SHOW TABLES LIKE 'Nurse'";
+$result = $connection->query($sql);
+if (!($result->num_rows > 0)) {
+	$sql = "CREATE TABLE ​Nurse 
+	(NurseID INT ​NOT NULL​,
+	FName   VARCHAR(15) ​NOT NULL​,
+	LName VARCHAR(15) ​NOT NULL​, 
+	DrWaitList INT,
+	PWaitTime Timestamp,
+	PRIMARY KEY ​(NurseID));"
+	$connection->query($sql);
+}
+
+$sql = "SHOW TABLES LIKE 'Patient'";
+$result = $connection->query($sql);
+if (!($result->num_rows > 0)) {
+	$sql = "CREATE TABLE Patient
+	(AHC CHAR(9) ​NOT NULL​,
+	FName VARCHAR(15) ​NOT NULL​,
+	LName VARCHAR(15) ​NOT NULL​,
+	SessionID INT, 
+	DrWaitList, 
+	PRIMARY KEY ​(AHC) ); "
+	$connection->query($sql);
+}
+
+$sql = "SHOW TABLES LIKE 'Session'";
+$result = $connection->query($sql);
+if (!($result->num_rows > 0)) {
+	$sql = "CREATE TABLE Session 
+	(SessionID INT
+	​NOT NULL​, 
+	Room VARCHAR(15) ​NOT NULL​,
+	TProvided VARCHAR(15),
+	DrID INT ​NOT NULL​,
+	PRIMARY KEY ​(SessionID), 
+	FOREIGN KEY ​(DrID) ​REFERENCES ​Doctor(DrID) ​ON DELETE ​SET NULL ​ON UPDATE CASCADE); "
+	$connection->query($sql);
+}
+
+$sql = "SHOW TABLES LIKE 'Waitlist'";
+$result = $connection->query($sql);
+if (!($result->num_rows > 0)) {
+	$sql = "CREATE TABLE Waitlist 
+	(DrID INT ​NOT NULL​,
+	PFName VARCHAR(15) ​NOT NULL​,
+	PLName VARCHAR(15) ​NOT NULL​,
+	PRIMARY KEY ​(DrID),
+	FOREIGN KEY ​(DrID) ​REFERENCES ​Doctor(DrID) ​ON DELETE ​CASCADE ​ON UPDATE CASCADE);"
+	$connection->query($sql);
+}
+
 $connection->close();
 ?>
 <html>
